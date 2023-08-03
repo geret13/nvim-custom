7299366d958873d0cb101d2e12324d04e08a1c38
Gerben van der Meer <gerben.vandermeer@maxem.energy>
2023-08-03 12:09:46 +0000

    initial commit

---
+++ configs/lspconfig.lua
@@ -0,0 +1,16 @@
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+local lspconfig = require "lspconfig"
+
+-- Server list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local servers = { "tsserver", "gopls", "golangci_lint_ls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
---
+++ plugins.lua
@@ -0,0 +1,16 @@
+local plugins = {
+
+  -- In order to modify the `lspconfig` configuration:
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+}
+
+return plugins
+
+
