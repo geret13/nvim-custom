ee3764923fb3b7e19ff7c7b534388490fa1fc644
Gerben van der Meer <gerben.vandermeer@maxem.energy>
2023-08-25 14:49:18 +0200

    Add rust_analyzer

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = configs.capabilities
 local lspconfig = require "lspconfig"
 
 -- Server list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-local servers = { "tsserver", "gopls", "golangci_lint_ls" }
+local servers = { "tsserver", "gopls", "golangci_lint_ls", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
